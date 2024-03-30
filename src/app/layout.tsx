aa76c9626241128abefed2258318f15174e085ef
Dario Reis <guttyerresjunior1995@gmail.com>
2024-03-30 15:28:23 -0300

    Feat: Configring favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
+  subsets: ['latin'],
+  variable: '--font-inter',
 })
 
 export const metadata: Metadata = {
-  title: "Cadastros Pessoas - Next.js",
-  description: "Cadastro de pessoas com Next.js",
-};
+  title: 'Cadastros Pessoas - Next.js',
+  description: 'Cadastro de pessoas com Next.js',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.variable}>{children}</body>
     </html>
-  );
+  )
 }
